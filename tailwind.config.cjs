17d3ebd17a49edb8c9f451ae9b25fcc421c8962d
devferx <ferkki@hotmail.com>
2023-02-07 18:25:53 -0400

    Add Navbar and icons components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-night": "#2A2A2A",
+        "grey-soft": "#CCCCCC",
+        primary: "#FFFF01",
+        malibu: "#4DCCFA",
+      },
+      fontFamily: {
+        anton: ["Anton", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+        burbank: ["Burbank Big Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
